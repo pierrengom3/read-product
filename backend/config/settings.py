c28aa8de3641bae377273efdab8d928661bf22ca
Pierre Ngom <ngompierre341@gmail.com>
2026-08-18 09:06:15 +0000

    Simplification config database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3')
     )
 }
 
